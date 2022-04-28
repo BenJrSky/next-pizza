e6578c70b783e3082a9accd5b7e8ee43543b3837
Benito Massidda <benjrsky@gmail.com>
2022-04-28 10:28:03 +0200

    - cart page added

---
+++ next-pizza/pages/cart.jsx
@@ -0,0 +1,66 @@
+
+import styles from '../styles/Cart.module.css';
+import Image from 'next/image';
+
+const Cart = ()=> {
+
+    return(
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <table className={styles.table}>
+                    <tr className={styles.tr}>
+                        <th className={styles.td}>Product</th>
+                        <th className={styles.td}>Name</th>
+                        <th className={styles.td}>Extras</th>
+                        <th className={styles.td}>Price</th>
+                        <th className={styles.td}>Quantity</th>
+                        <th className={styles.td}>Total</th>
+                    </tr>
+                    <tr className={styles.tr}>
+                        <td>
+                            <div className={styles.imgContainer}>
+                                <Image src='/img/pizza.png' layout='fill' objectFit='cover'/>
+                            </div>
+                        </td>
+                        <td>
+                            <span className={styles.name}>CORALZO</span>
+                        </td>
+                        <td>
+                            <span className={styles.extras}>Double ingredients, spicy sauce</span>
+                        </td>
+                        <td>
+                            <span className={styles.price}>$19.90</span>
+                        </td>
+                        <td>
+                            <span className={styles.quantity}>2</span>
+                        </td>
+                        <td>
+                            <span className={styles.total}>$39.80</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div className={styles.right}>
+                <div className={styles.wrapper}>
+                    <h2 className={styles.title}>CART TOTAL</h2>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Total:</b>$79.60
+                    </div>
+                    <button className={styles.button}>CHECKOUT NOW!</button>
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+
+
+
+export default Cart
