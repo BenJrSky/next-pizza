9b5b05edc4df3a09b935eb5b9b0f12fb0b74b605
Benito Massidda <benjrsky@gmail.com>
2022-04-30 22:56:30 +0200

    - models added;

---
+++ next-pizza/utilities/mongo.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const MONGO_URL = process.env.MONGO_URL;
+
+if(!MONGO_URL){
+    throw new Error('Please define the MONGO_URL enviroment variable inside .env');
+}
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = {conn:null, promise:null};
+}
+
+async function dbConncect(){
+    if(cached){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands:false
+        }
+
+        cached.promise = mongoose.connect(MONGO_URL,opts).then(mongoose=>{
+            return mongoose
+        });
+    }
+
+    cached.conn = await cached.promise
+    return cached.conn
+
+}
+
+export default dbConncect
